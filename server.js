322835d3708700618c316caeb14f723f9415e289
aliabdelhai <ali.abdalhay@mail.huji.ac.il>
2021-03-26 16:21:16 +0300

    update file

--- server.js
+++ server.js
@@ -6,23 +6,28 @@ const mongoose = require('mongoose')
 const api = require('./server/routes/api')
 
 // mongoose.connect('mongodb://localhost/crm-mongo', { useNewUrlParser: true })
+PORT = process.env.PORT || 5000,
+URI = process.env.MONGODB_URI || 'mongodb://localhost/crm-mongo',
 
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/crm-mongo');
 
 
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('node_modules'));
+app.use(express.static('build'));
+app.use('/', api);
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, connectTimeoutMS: 5000, serverSelectionTimeoutMS: 5000 })
+.then(function(){
+    app.listen(PORT, function(){
+        console.log(`Server is up and running on port: ${PORT}`);
+    });
 })
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use('/', api)
-
-
-const PORT = 8080
-app.listen(process.env.PORT || PORT);
-
-
+.catch(function(err){
+    console.log(err.message);
+});
