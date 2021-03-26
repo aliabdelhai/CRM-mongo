3db62a00ab1594d5c88985e8336ac627ba8ccc73
aliabdelhai <ali.abdalhay@mail.huji.ac.il>
2021-03-26 16:05:09 +0300

    update file

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -19,7 +19,7 @@ function Clients(props) {
     }
 
     useEffect(async () => {
-        let clientsData = await axios.get('http://localhost:4200/clients')
+        let clientsData = await axios.get('/clients')
         props.clientsStore.clients = clientsData.data
     }, [])
 
