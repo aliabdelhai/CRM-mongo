0e4991f470138a323ac66ab0a89ea467652d81ac
aliabdelhai <ali.abdalhay@mail.huji.ac.il>
2021-03-27 14:49:36 +0300

    push

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const api = require('./server/routes/api')
 const PORT = process.env.PORT || 4200
 const URI = process.env.MONGODB_URI || 'mongodb://localhost/crm-mongo'
 
+const uri = "mongodb+srv://AliAbed:hL6QVC99JuYjB9wh@cluster0.g0jq7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, connectTimeoutMS: 5000, serverSelectionTimeoutMS: 5000 })
+.then(function(){
+    console.log("Db connected")
+})
+.catch(function(err){
+    console.log(err.message);
+});
 
 
 
@@ -18,17 +26,11 @@ app.use(express.static('node_modules'));
 app.use(express.static('build'));
 app.use('/', api);
 
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const uri = "mongodb+srv://AliAbed:hL6QVC99JuYjB9wh@cluster0.g0jq7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, connectTimeoutMS: 5000, serverSelectionTimeoutMS: 5000 })
-.then(function(){
-    app.listen(PORT, function(){
-        console.log(`Server is up and running on port: ${PORT}`);
-    });
-})
-.catch(function(err){
-    console.log(err.message);
+app.listen(PORT, function(){
+    console.log(`Server is up and running on port: ${PORT}`);
 });
